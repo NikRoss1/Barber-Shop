2fcbe6e3305cc972a9945d218bfe87af50d4c52e
Nikolay Panov <nik.cmkw@yandex.ru>
2022-08-17 01:10:01 +0500

    init Barbers table on vonfigure

--- app.rb
+++ app.rb
@@ -2,6 +2,19 @@ require 'rubygems'
 require 'sinatra'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+	db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+	
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+
+			db.execute 'insert into Barbers (name) values (?)', [barber]
+		end
+	end
+end
 
 configure do
   db = SQLite3::Database.new 'barbershop.db'
@@ -14,6 +27,13 @@ configure do
       "barber" TEXT,
       "color" TEXT
     )'
+  db.execute 'CREATE TABLE IF NOT EXISTS "Barbers"
+    (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "name" TEXT
+      )'
+  seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Joker'] 
+
 end
 
 get '/' do
